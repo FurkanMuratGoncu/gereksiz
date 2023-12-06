ff0bb46f1878609921437aa740bbe586dfc11ba7
FurkanMuratGoncu <153122498+FurkanMuratGoncu@users.noreply.github.com>
2023-12-06 21:38:19 +0300

    Add files via upload

---
+++ hebele hübele/ConsoleApp5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+    {
+        internal class RandomSayi
+        {
+            public int rendemsayiüretci()
+            {
+                return new Random().Next(1, 11);
+            }
+        }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomSayi randomSayi = new RandomSayi();
+            int rendem = randomSayi.rendemsayiüretci();
+            Console.WriteLine("1 ile 10 arasında bir sayı seçildi. Tahmininizi girin.");
+            //  int insn;
+            //insn=Convert.ToInt32(Console.ReadLine());
+            int insn;
+            bool dogruTahmin = false;
+
+            while (!dogruTahmin)
+            {
+                insn = Convert.ToInt32(Console.ReadLine());
+
+                if (insn == rendem)
+                {
+                    Console.WriteLine("Tebrikler! Doğru tahmin ettiniz.");
+                    dogruTahmin = true;
+                }
+                else
+                {
+                    Console.WriteLine("Tekrar deneyiniz. Yanlış tahmin.");
+                }
+               
+            }
+           
+        }
+    }  }
+
+
+   
+
